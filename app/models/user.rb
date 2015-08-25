2575b3f3f80e6d541e9df918259e11f6ddf5aba9
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-08-25 17:38:17 +0900

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
 
   attr_accessor :user_name, :image, :member, :profile, :work
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, authentication_keys: [:email, :user_name, :image, :member, :profile, :work]
+         :recoverable, :rememberable, :trackable, :validatable
 
 end
