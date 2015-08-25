33388008526a30435ecdeaadd8806368024230e0
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-08-25 16:06:33 +0900

    add sign_up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  attr_accessor :user_name, :image, :member, :profile, :work
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, authentication_keys: [:email, :user_name, :image, :member, :profile, :work]
+
 end
