b29615505752f14af53be961b02a452352e5bbce
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-08-26 16:50:50 +0900

    add post_prototype

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,16 @@
+class PrototypesController < ApplicationController
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to root_path
+  end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept)
+  end
+end
