8ff63f9b2dccec6a4e7f7a4930b48970416763cf
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-08-28 01:32:23 +0900

    fix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,7 +7,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = current_user.new(create_params)
+    @prototype = current_user.prototypes.new(create_params)
     if @prototype.save
       redirect_to root_path
     else
