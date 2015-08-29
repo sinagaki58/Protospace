9432d92d3728bba680a156c133d2468038b71db7
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-08-29 09:33:16 +0900

    add association

---
+++ app/models/prototype.rb
@@ -0,0 +1,8 @@
+class Prototype < ActiveRecord::Base
+
+  belongs_to :user
+
+  has_many :thumbnails
+
+  accepts_nested_attributes_for :thumbnails
+end
--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,4 +1,6 @@
 class Thumbnail < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
+
+  belongs_to :prototype
 end
