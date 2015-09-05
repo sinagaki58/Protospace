50008781501047b42511263c6821f386cab7d958
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-09-05 22:41:49 +0900

    add before action to set_prototype

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,12 +1,18 @@
 class Prototypes::CommentsController < ApplicationController
 
+  before_action :set_prototype, only: :create
+
   def create
     current_user.comments.create(comment_params)
-    @prototype = Prototype.find(comment_params[:prototype_id])
     @comments = @prototype.comments
   end
 
 private
+
+  def set_prototype
+    @prototype = Prototype.find(comment_params[:prototype_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:text, :prototype_id, :user_id)
   end
