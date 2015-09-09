09f79c2b8f228c1e0cae73efda6b0e555813198e
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-09-09 14:16:45 +0900

    Revert "add destroy action in route"
    This reverts commit e8f3089668964ba6c3c4f84ba2b8abeaceadbd9f.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   resources :tags, only: [:index, :show]
-  resources :likes, only: [:create, :destroy]
+  resources :likes, only: :create, param: :prototype_id
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
