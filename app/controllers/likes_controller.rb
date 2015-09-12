a6a6870d296bc8957c3b024243d25d8c1964e262
稲垣秀一朗 <s.inagaki58@gmail.com>
2015-09-13 01:36:07 +0900

    remove redirect

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,11 @@ class LikesController < ApplicationController
 
   def create
     Like.create(create_params)
-    redirect_to :back
   end
 
   def destroy
     like = Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id)
     like.destroy
-    redirect_to :back
   end
 
 private
